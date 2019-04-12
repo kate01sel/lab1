b706587c202536e33c6dd90338ba49916d021963
lady01key@gmail.com <lady01key@gmail.com>
2019-04-12 12:28:19 +0300

    готово

---
+++ Lab1 Csharp/Lab1 Csharp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+
+namespace Квадрат_разности
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int x = 0;
+            int y = 0;
+            double rez = -1;
+            x = Convert.ToInt32(Console.ReadLine());
+            y = Convert.ToInt32(Console.ReadLine());
+            rez = kv(x, y);
+            Console.WriteLine(rez);
+        }
+
+        public static double kv(int x, int y)
+        {
+            return Math.Pow((x - y), 2);
+        }
+
+    }
+}
